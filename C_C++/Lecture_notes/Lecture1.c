eb78ebfa04647942d6dbd60b0ba3d6987b9e226c
KyraZzz <yz709@cam.ac.uk>
2021-06-21 22:38:35 +0100

    Lecture 1 done

--- C_C++/Lecture_notes/Lecture1.c
+++ C_C++/Lecture_notes/Lecture1.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
+int a;                 // not initialised - not sure about the exact value
+unsigned char b = 'A'; // declared and defined
+extern int alpha;      // declared but not defined, tell the compiler it is defined elsewhere
+
+char s[] = "University of Cambridge Computer Laboratory";
 
 int main(void)
 {
-    int a, b;
-    a = b = 3;
-    printf("%d, %d\n", a, b);
+    extern unsigned char b;           // b already defined so no change
+    double a = 3.4;                   // may get warning from compiler: redefined and declared a
+    {                                 // scope zoom into block
+        extern a;                     // type specifier missing, defaults to 'int'
+        printf("%d %d \n", b, a + 1); // 65 1, a refer to global int a
+    }                                 // scope zoom out of block
+
+    for (int i = 0, j = strlen(s) - 1; i < j; i++, j--) // strlen excludes the end '\0'
+    {
+        char c;
+        c = s[i], s[i] = s[j], s[j] = c; // swap the two chars
+    }
+    printf("%s\n", s);
     return 0;
 }
