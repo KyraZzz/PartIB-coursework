7f235d680c81c9a6f3efeea1117ddb6dcca1f57c
KyraZzz <yz709@cam.ac.uk>
2022-05-01 23:22:30 +0100

    add instructions for CLISP files

---
+++ CPL/Exp.java
@@ -0,0 +1,38 @@
+package CPL;
+
+public class Exp {
+    private int tag;
+
+    Exp(int tag) {
+        this.tag = tag;
+    }
+}
+
+class Var extends Exp {
+    private char var_name;
+
+    public Var(char var_name) {
+        super(1);
+        this.var_name = var_name;
+    }
+}
+
+class Neg extends Exp {
+    private Exp arg;
+
+    public Neg(Exp arg) {
+        super(2);
+        this.arg = arg;
+    }
+}
+
+class Divide extends Exp {
+    private Exp arg1;
+    private Exp arg2;
+
+    public Divide(Exp arg1, Exp arg2) {
+        super(3);
+        this.arg1 = arg1;
+        this.arg2 = arg2;
+    }
+}
